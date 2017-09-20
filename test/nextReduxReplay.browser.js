e972a57e399c1bea01f27b24460d0161cc185061
William Cunningham <w.a.cunningham.ii@gmail.com>
2017-09-19 20:11:08 -0400

    Add browser tests

---
+++ test/nextReduxReplay.browser.js
@@ -0,0 +1,57 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { shallow } from "enzyme";
+import { createElement } from "react";
+import nextReduxReplay from "../src/nextReduxReplay";
+
+const noop = () => {};
+
+test("getInitialProps() returns expected actions", () => {
+  const actions = [{ type: "SOME_ACTION" }, { type: "OTHER_ACTION" }];
+  const callCreateStore = createStore => createStore(noop);
+  const setup = store => {
+    actions.forEach(action => store.dispatch(action));
+    return Promise.resolve();
+  };
+  const hoc = nextReduxReplay(callCreateStore, setup);
+  const { getInitialProps } = hoc(noop);
+
+  expect.assertions(1);
+  return getInitialProps().then(value => expect(value).toEqual({ actions }));
+});
+
+test("renders provided element with expected props", () => {
+  const callCreateStore = createStore => createStore(noop);
+  const setup = () => Promise.resolve();
+  const hoc = nextReduxReplay(callCreateStore, setup);
+  const component = props =>
+    createElement("div", { ...props, id: "my-component" }, "Hello!");
+  const wrappedComponent = hoc(component);
+  wrappedComponent.getInitialProps();
+  const props = { a: "a", b: "b" };
+
+  const element = shallow(createElement(wrappedComponent, props));
+  expect(element.find("#my-component").props()).toMatchObject(props);
+});
+
+describe("when `getInitialProps()` not called", () => {
+  test("throws error when actions not provided", () => {
+    const callCreateStore = createStore => createStore(noop);
+    const setup = () => Promise.resolve();
+    const hoc = nextReduxReplay(callCreateStore, setup);
+    const wrappedComponent = hoc(() => createElement("div"));
+    expect(() => shallow(createElement(wrappedComponent))).toThrow();
+  });
+
+  test("doesn't throw if actions provided", () => {
+    const callCreateStore = createStore => createStore(noop);
+    const setup = () => Promise.resolve();
+    const hoc = nextReduxReplay(callCreateStore, setup);
+    const wrappedComponent = hoc(() => createElement("div"));
+    expect(() =>
+      shallow(createElement(wrappedComponent, { actions: [] }))
+    ).not.toThrow();
+  });
+});
